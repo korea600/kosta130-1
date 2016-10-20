cd316fb67778ca9cf58e2e803153f14b9183e065
DaewonLee <DaewonLee@192.168.0.102>
2016-10-20 14:11:38 +0900

    Login Action 구현 완료

--- Project2/src/member/actions/LoginAction.java
+++ Project2/src/member/actions/LoginAction.java
@@ -20,23 +20,27 @@ public class LoginAction extends Action {
 		// 입력된 파라미터 얻어오기 (id,비번)
 		String id=request.getParameter("id");
 		String pass=request.getParameter("pass");
-		String job="";
-		switch (request.getParameter("job")) {
-			case "학생"		:	job="S";break;
-			case "교수"		:	job="P";break;
-			case "교직원"		:	job="A";break;
+		String job=request.getParameter("job");
+		
+		// 입력된 job 파라미터를 DB용 코드 데이터로 변경
+		switch (job) {
+			case "student"	:	job="S";break;
+			case "professor":	job="P";break;
+			case "admin"	:	job="A";break;
 		}
+		
+		// id,비번,구분 일치 여부 확인 (일치시 dto얻어오기, 불일치시 null)
 		LoginDTO dto = new LoginDTO(id,pass,null,job);
 		LoginDTO checked_dto = dao.login(dto);
-		System.out.println(checked_dto.getId()+checked_dto.getName()+checked_dto.getJob());
-		// id에 해당하는 비번이 맞는지 확인
+		
+		// 정보가 일치하여 dto를 얻어오면 dto 정보를 세션에 저장
 		if(checked_dto!=null){		// id,비번 확인 통과		
 			checked_dto.setPass("");	// 세션에 저장될 DTO값중 password 지우기
-			request.getSession().setAttribute("LoginDTO", checked_dto);// 여기까지는 오류 없음 확인 완료
-			request.setAttribute("login_result", 1);
+			request.getSession().setAttribute("LoginDTO", checked_dto);
+			request.setAttribute("login_result", "true");	// 로그인 성공 메시지 ajax용으로 출력
 		}
-		else{		// id,비번 확인 실패
-			request.setAttribute("login_result", 0);
+		else{
+			request.setAttribute("login_result", "false");	// 로그인 실패 메시지 ajax용으로 출력
 		}
 		return mapping.findForward("result");
 	}
