53c76254257b0a3a024efa4c1e5c0a036db37276
DaewonLee <DaewonLee@192.168.0.102>
2016-11-02 15:13:20 +0900

    null에러 수정 (파일 누락 ㅋ)

--- Project2/src/common/actions/FirstPageAction.java
+++ Project2/src/common/actions/FirstPageAction.java
@@ -24,30 +24,31 @@ public class FirstPageAction extends Action{
 		String action = request.getParameter("action");
 		// 공지사항 얻어오기 (학사/취업)
 		
-		if(action.equals("login")){
+		if(action==null || action.equals("login")){
 			NoticeDAO notice = new NoticeDAO();
 			List<NoticeDTO> notice_haksa = notice.select("학사"); 
 			List<NoticeDTO> notice_job = notice.select("취업");
 			request.setAttribute("haksa_list", notice_haksa);
 			request.setAttribute("job_list", notice_job);
-		// 학사일정 얻어오기
-		List<ScheduleDTO> schedule = new ScheduleDAO().recentList();
-		request.setAttribute("list", schedule);
+			// 학사일정 얻어오기
+			List<ScheduleDTO> schedule = new ScheduleDAO().recentList();
+			request.setAttribute("list", schedule);
 		
 
 		
 		// 로그인 분류에 따라 페이지 출력 구분 하기 (학생, 교수, 교직원)
-	LoginDTO dto = (LoginDTO) request.getSession().getAttribute("LoginDTO");
-		String job = dto.getJob();
-		switch (job) {
-			case "S" :
-				forward = mapping.findForward("student");break;
-			case "P" :
-				forward = mapping.findForward("professor");break;
-			case "A" :
-				forward = mapping.findForward("admin");break;
+			LoginDTO dto = (LoginDTO) request.getSession().getAttribute("LoginDTO");
+			String job = dto.getJob();
+			switch (job) {
+				case "S" :
+					forward = mapping.findForward("student");break;
+				case "P" :
+					forward = mapping.findForward("professor");break;
+				case "A" :
+					forward = mapping.findForward("admin");break;
 			}//switch
-		}else if(action.equals("main")){
+		}
+		else if(action.equals("main")){
 			NoticeDAO notice = new NoticeDAO();
 			List<NoticeDTO> notice_haksa = notice.select("학사"); 
 			List<NoticeDTO> notice_job = notice.select("취업");
